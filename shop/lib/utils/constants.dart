75a096253da79203fee4af32c043a4a6c635b3c4
Thyago Henrique <thyagobs.al@gmail.com>
2024-03-07 08:38:15 -0300

    commit

--- shop/lib/utils/constants.dart
+++ shop/lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const productBaseUrl =
-      'https://shop-cod3r-e21d3-default-rtdb.firebaseio.com/products';
+      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl =
-      'https://shop-cod3r-e21d3-default-rtdb.firebaseio.com/orders';
+      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/orders';
 }
